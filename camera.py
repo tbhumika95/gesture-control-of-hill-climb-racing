bb3bfa01870772d08267e0f811e7eed20f8c2d8f
Bhumika Tiwari <tbhumika95@gmail.com>
2026-05-28 02:54:51 +0530

    Add real-time hand landmark detection using MediaPipe

--- camera.py
+++ camera.py
@@ -1,7 +1,29 @@
 import cv2
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+
+mp_draw = mp.solutions.drawing_utils
+
 cap = cv2.VideoCapture(0)
 while True :
     success , frame = cap.read()
+
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = hands.process(rgb_frame)
+
+    if results.multi_hand_landmarks:
+
+        for hand_landmarks in results.multi_hand_landmarks:
+
+            # Draw landmarks and connections
+            mp_draw.draw_landmarks(
+                frame,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS
+            )
+
 
     cv2.imshow("Camera" , frame)
 
